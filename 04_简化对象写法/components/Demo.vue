5369a2bd4ea8f762abd386982bf36214ee42549f
wuaiyousan <1163702239@qq.com>
2023-09-11 15:37:02 +0800

    feat[src]:简化对象写法，箭头函数

---
+++ 04_简化对象写法/components/Demo.vue
@@ -0,0 +1,46 @@
+<template>
+  <h3>简化对象写法</h3>
+  <button @click="copyChagen">按钮</button>
+</template>
+
+<script setup>
+  import { reactive } from "vue";
+
+  // ES6 允许在大括号里，直接写入变量和函数，作为对象的属性和方法。书写更加简洁
+  let name = 'xhy'
+  let change = function(){
+    console.log('22222');
+  }
+
+  let person = reactive({
+    name,
+    age: 29,
+    handler: change,
+    improve(){
+      console.log('33333');
+    }
+  })
+
+  person.handler()
+
+  function copyChagen(){
+    console.log(person);
+    person.improve()
+  }
+
+  // ---案例---
+
+</script>
+
+<style>
+  .list{
+    display: flex;
+    flex-direction: row;
+  }
+  .item{
+    width: 60px;
+    height: 30px;
+    border: 1px solid #000;
+    margin-left: 10px;
+  }
+</style>
