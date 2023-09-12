5fa49bc4f44faccdff225ef8c1b93e25c01ffeae
wuaiyousan <1163702239@qq.com>
2023-09-12 16:03:29 +0800

    feat[src]:函数参数的默认设置，rest参数，扩展运算符

---
+++ 06_函数参数的默认设置/components/Demo.vue
@@ -0,0 +1,35 @@
+<template>
+  <h3>函数参数默认值</h3>
+  <h3>{{sum}}</h3>
+</template>
+
+<script setup>
+  // ES6 允许给函数参数赋值初始值
+
+  // 1.形参的初始值。 具有默认值的参数，一般位置要靠后
+  function add(a, b, c=10){
+    return a + b + c;
+  }
+  let sum = add(1, 2)
+
+  // 2.可以与解构赋值一起使用
+  function person({name, age=29}){
+    console.log(name,age);
+  }
+  person({
+    name: 'xhy',
+  })
+</script>
+
+<style>
+  .list{
+    display: flex;
+    flex-direction: row;
+  }
+  .item{
+    width: 60px;
+    height: 30px;
+    border: 1px solid #000;
+    margin-left: 10px;
+  }
+</style>
---
+++ 07_rest参数/components/Demo.vue
@@ -0,0 +1,44 @@
+<template>
+  <h3>rest参数</h3>
+</template>
+
+<script setup>
+  // ES6 提供了rest参数，用于获取函数的实参，用来替代arguments
+
+  // ES5获取实参
+  function person(){
+    console.log(arguments);
+  }
+  person('xhy','lb','ck') // 输出为对象
+
+  // ES6 rest参数获取实参
+  function car(...other){
+    console.log(other);
+  }
+  car('byd','bc') // 输出为函数
+
+  // rest参数必须要放到参数的最后
+  // function rooms(a, b, c, ...other){
+  //   console.log(a, b, c, other);
+  // }
+  // rooms('101', '102', '103', 104, 105, 106)
+
+  // 
+  let school = (...args)=>console.log(args);
+  
+  school('jsdx','zndx')
+
+</script>
+
+<style>
+  .list{
+    display: flex;
+    flex-direction: row;
+  }
+  .item{
+    width: 60px;
+    height: 30px;
+    border: 1px solid #000;
+    margin-left: 10px;
+  }
+</style>
